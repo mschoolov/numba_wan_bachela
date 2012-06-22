40a0891338bc187eb25bf85ac5be55d7e7993676
Misha Schoolov <mschoolov@gmail.com>
2012-06-21 22:31:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NumbaWanBachela::Application.config.secret_token = '8a6306e854784acf9d50ec111bffae61ae377ee4534eecabd125c99d686ac3df28102999040c74771acade526bb20b615e17fbab873a73273795e426b7e967aa'
